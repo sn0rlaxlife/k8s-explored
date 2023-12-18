9aa830242f651d6ec6950c8fd0fb09f0c0d7a124
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-12-18 13:26:30 -0800

    Update k8s-kind.sh

--- k8s-kind.sh
+++ k8s-kind.sh
@@ -3,10 +3,9 @@
 kind create cluster --config=- <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
-featureGates:
-  ValidatingAdmissionPolicy: true
 runtimeConfig:
-  admissionregistration.k8s.io/v1beta1
+  "admissionregistration.k8s.io/v1beta1": true
+  "api/beta": true
 networking:
   ipFamily: ipv6
   apiServerAddress: 127.0.0.1
@@ -14,15 +13,17 @@ networking:
 name: ambient
 nodes:
 - role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: ClusterConfiguration
+    apiServer:
+        extraArgs:
+          feature-gates: "ValidatingAdmissionPolicy=true"
+          enable-admission-plugins: NodeRestriction,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ValidatingAdmissionPolicy
   image: kindest/node:v1.29.0@sha256:eaa1450915475849a73a9227b8f201df25e55e268e5d619312131292e324d570
   extraPortMappings:
   - containerPort: 80
     hostPort: 80
     listenAddress: "127.0.0.1"
     protocol: TCP
-- role: worker
-  image: kindest/node:v1.29.0@sha256:eaa1450915475849a73a9227b8f201df25e55e268e5d619312131292e324d570
-- role: worker
-  image: kindest/node:v1.29.0@sha256:eaa1450915475849a73a9227b8f201df25e55e268e5d619312131292e324d570
-
 EOF
