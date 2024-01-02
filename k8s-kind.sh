b4f5b070748655f1ecfc98bd5385a11ab7a6e7a2
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2024-01-02 09:31:32 -0800

    Update k8s-kind.sh

--- k8s-kind.sh
+++ k8s-kind.sh
@@ -3,8 +3,9 @@
 kind create cluster --config=- <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+featureGates:
+  "ValidatingAdmissionPolicy": true
 runtimeConfig:
-  "admissionregistration.k8s.io/v1beta1": true
   "api/beta": true
 networking:
   ipFamily: ipv6
@@ -13,13 +14,6 @@ networking:
 name: ambient
 nodes:
 - role: control-plane
-  kubeadmConfigPatches:
-  - |
-    kind: ClusterConfiguration
-    apiServer:
-        extraArgs:
-          feature-gates: "ValidatingAdmissionPolicy=true"
-          enable-admission-plugins: NodeRestriction,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ValidatingAdmissionPolicy
   image: kindest/node:v1.29.0@sha256:eaa1450915475849a73a9227b8f201df25e55e268e5d619312131292e324d570
   extraPortMappings:
   - containerPort: 80
