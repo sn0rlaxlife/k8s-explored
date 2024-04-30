1b2d23e84ba254986948a185f5f4cad5dd2317a9
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2024-04-30 08:03:28 -0700

    Create k8s-kind.sh

---
+++ v/k8s-kind.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+kind create cluster --config=- <<EOF
+kind: Cluster
+name: test-cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+featureGates:
+  ValidatingAdmissionPolicy: true
+runtimeConfig:
+  "admissionregistration.k8s.io/v1beta1": true
+networking:
+  ipFamily: ipv6
+  apiServerAdress: 127.0.0.1
+  apiServerPort: 6443
+name: ambient
+nodes:
+- role: control-plane
+  image: kindest/node:v1.30.0-alpha.0.102_41890534532931@sha256:446bf1e60d4365b8b37ef832e99a8377cd41178889d86275880667e8034c89d9
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    listenAddress: "127.0.0.1"
+    protocol
+EOF
